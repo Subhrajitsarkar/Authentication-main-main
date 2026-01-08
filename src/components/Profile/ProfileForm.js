3e6161e385da2b93f50c5e050d59b2aae0100c32
Subhrajitsarkar <subhrajitsarkar28@gmail.com>
2026-01-09 00:50:28 +0530

    Updating the Old Password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,9 +1,11 @@
 import React, { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory();
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -25,6 +27,7 @@ const ProfileForm = () => {
       }
     }).then(res => {
       //assume always success
+      history.replace('/');
     });
   }
 
